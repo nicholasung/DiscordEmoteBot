9fa44c8cf3150993347cde40692c6ba27b1e86d5
nicholasung <nicholas.ung0@gmail.com>
2023-10-31 21:47:57 -0700

    Now works with more than one server simultaneously

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from discord.ext import commands
+from discord import app_commands
 import discord
 import os
 
@@ -14,6 +15,7 @@ intents.guild_messages = True
 intents.guild_reactions = True
 intents.message_content = True
 global most_recent
+most_recent = [] #array of dictionary pairs (guild, message)
 bot = commands.Bot(command_prefix='/', intents=intents)
 
 # event: when the bot is ready
@@ -21,23 +23,27 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 async def on_ready():
     print('We have logged in')
 
-# event: look for the most recent image with the caption "&add" and set as most recent
-# @bot.event
-# async def on_message(m):
-#     if m.content == "$add":
-#         if len(m.attachments) == 1: 
-#             if m.attachments[0].content_type[0:5] == "image/":
-#                 most_recent = m
-#                 print(most_recent)
-#     await bot.process_commands(m) 
+def check_key(key): #return the pair of the guild with most recent
+    for pair in most_recent:
+        if key == pair.get("gid"):
+            print("found")
+            return pair
+    print("no pair")
+    return -1
+        
+
+
 @bot.event
 async def on_message(m):
     if m.content.startswith("$add"):
         if len(m.attachments) == 1:
             if m.attachments[0].content_type.startswith("image/"):
-                global most_recent
-                most_recent = m
-                print(most_recent)
+                if len(most_recent) != 0:
+                     for i in range(len(most_recent)):
+                        if i < len(most_recent) and m.guild.id == most_recent[i].get("gid"):
+                            most_recent.pop(i)
+                most_recent.append(dict(gid = m.guild.id, msg = m)) #add a new most recent: Must now find a way to delete old recents for the guild if they exist (just have it delete on a timer)
+            print(most_recent)
     await bot.process_commands(m)
 
 # command: status
@@ -50,18 +56,20 @@ async def status(ctx):
 async def emotify(ctx, name):
     image = None
     if ctx.author.top_role.permissions.create_expressions: #NEEDS TO EXPLICITLY HAVE MANAGE_EMOJIS, CANNOT BE INHERITED
-        if most_recent != None: #is a message
-            image = await most_recent.attachments[0].read()
+        if check_key(ctx.guild.id) != -1: #is a message
+            image = await check_key(ctx.guild.id).get("msg").attachments[0].read()
+        else: 
+            await ctx.send('There is no valid emote candidate')
     else: 
-       await ctx.send('Either no Emoji Permissions or there is no valid emote candidate')
+       await ctx.send('You do not have emoji creation permissions')
 
-    guild_emojis = await ctx.guild.fetch_emojis()
+    guild_emojis = await ctx.guild.fetch_emojis() #emjoi limit checks
 
-    if len(guild_emojis) >= ctx.guild.emoji_limit:
+    if len(guild_emojis) >= ctx.guild.emoji_limit: 
        await ctx.send('Too many Emojis, no more space!')
        return 
     
     if image != None:
-        await ctx.guild.create_custom_emoji(name=name, image=image)
+        await ctx.guild.create_custom_emoji(name=name, image=image) #create emoji
 
 bot.run(token)
